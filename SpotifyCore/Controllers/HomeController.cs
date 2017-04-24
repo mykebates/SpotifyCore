6cd28b0d81727997b9546346276014b1d8d63923
Myke Bates <mbates@thealchemediaproject.com>
2017-04-24 17:41:24 -0600

    initial commit

---
+++ SpotifyCore/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using SpotifyCore.Models;
+
+namespace SpotifyCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SearchResults(String search)
+        {
+            var client = new RestClient("https://api.spotify.com/v1");
+            var request = new RestRequest("search", Method.GET);
+            request.AddParameter("limit", 3);
+            request.AddParameter("type", "artist,track,album,playlist");
+            request.AddParameter("q", search);
+
+            TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+
+            var asyncClient = client.ExecuteAsync(request, r =>
+            {
+                taskCompletion.SetResult(r);
+            });
+
+            RestResponse response = (RestResponse)(await taskCompletion.Task);
+
+            var searchData = JsonConvert.DeserializeObject<SpotifySearch>(response.Content);
+
+            return View("SearchResults", searchData);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ SpotifyCore/Models/SpotifySearch.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotifyCore.Models
+{
+    public class SpotifySearch
+    {
+        public Albums albums { get; set; }
+        public Artists artists { get; set; }
+        public Tracks tracks { get; set; }
+        public Playlists playlists { get; set; }
+    }
+
+    public class ExternalUrls
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Artist
+    {
+        public ExternalUrls external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class ExternalUrls2
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Image
+    {
+        public int height { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Item
+    {
+        public string album_type { get; set; }
+        public List<Artist> artists { get; set; }
+        public List<string> available_markets { get; set; }
+        public ExternalUrls2 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public List<Image> images { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class Albums
+    {
+        public string href { get; set; }
+        public List<Item> items { get; set; }
+        public int limit { get; set; }
+        public string next { get; set; }
+        public int offset { get; set; }
+        public object previous { get; set; }
+        public int total { get; set; }
+    }
+
+    public class ExternalUrls3
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Followers
+    {
+        public object href { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Image2
+    {
+        public int height { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Item2
+    {
+        public ExternalUrls3 external_urls { get; set; }
+        public Followers followers { get; set; }
+        public List<string> genres { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public List<Image2> images { get; set; }
+        public string name { get; set; }
+        public int popularity { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class Artists
+    {
+        public string href { get; set; }
+        public List<Item2> items { get; set; }
+        public int limit { get; set; }
+        public object next { get; set; }
+        public int offset { get; set; }
+        public object previous { get; set; }
+        public int total { get; set; }
+    }
+
+    public class ExternalUrls4
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Artist2
+    {
+        public ExternalUrls4 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class ExternalUrls5
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Image3
+    {
+        public int height { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Album
+    {
+        public string album_type { get; set; }
+        public List<Artist2> artists { get; set; }
+        public List<string> available_markets { get; set; }
+        public ExternalUrls5 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public List<Image3> images { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class ExternalUrls6
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Artist3
+    {
+        public ExternalUrls6 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class ExternalIds
+    {
+        public string isrc { get; set; }
+    }
+
+    public class ExternalUrls7
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Item3
+    {
+        public Album album { get; set; }
+        public List<Artist3> artists { get; set; }
+        public List<string> available_markets { get; set; }
+        public int disc_number { get; set; }
+        public int duration_ms { get; set; }
+        public bool @explicit { get; set; }
+        public ExternalIds external_ids { get; set; }
+        public ExternalUrls7 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public int popularity { get; set; }
+        public string preview_url { get; set; }
+        public int track_number { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class Tracks
+    {
+        public string href { get; set; }
+        public List<Item3> items { get; set; }
+        public int limit { get; set; }
+        public string next { get; set; }
+        public int offset { get; set; }
+        public object previous { get; set; }
+        public int total { get; set; }
+    }
+
+    public class ExternalUrls8
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Image4
+    {
+        public int height { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+    }
+
+    public class ExternalUrls9
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Owner
+    {
+        public ExternalUrls9 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class Tracks2
+    {
+        public string href { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Item4
+    {
+        public bool collaborative { get; set; }
+        public ExternalUrls8 external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public List<Image4> images { get; set; }
+        public string name { get; set; }
+        public Owner owner { get; set; }
+        public object @public { get; set; }
+        public string snapshot_id { get; set; }
+        public Tracks2 tracks { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+
+    public class Playlists
+    {
+        public string href { get; set; }
+        public List<Item4> items { get; set; }
+        public int limit { get; set; }
+        public string next { get; set; }
+        public int offset { get; set; }
+        public object previous { get; set; }
+        public int total { get; set; }
+    }
+}
